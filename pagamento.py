e5ef51bee6b52b23c9a6c9f98c4a17d1c9c11f79
andreaBicher <89815459+andrea-prestini@users.noreply.github.com>
2023-01-12 14:27:20 +0100

    fix errori battitura

--- pagamento.py
+++ pagamento.py
@@ -4,7 +4,7 @@ import time
 def accettato(value: int) -> bool:
     accettato = value > 0
 
-    print("Hai richeisto un pagamento di", value)
+    print("Hai richiesto un pagamento di", value)
     time.sleep(3)
     if accettato:
         print("Pagamento accettato...")
