3ba1ad0ee56630c3d61a98093c3e61b2d0d8767e
andreaBicher <89815459+andrea-prestini@users.noreply.github.com>
2023-01-13 01:48:15 +0100

    add wrapper GPT solution for web link

--- my_solution_GPT.py
+++ my_solution_GPT.py
@@ -1,13 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+
 
 # Send a GET request to the website and retrieve the HTML content
 url = 'https://books.toscrape.com/'
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
-
 book_containers = soup.find_all("article", class_="product_pod")
-for container in book_containers:
-    title = container.h3.a.text
-    price = container.find("div", class_="product_price").p.text
-    print(title, price)
+
+# Open a CSV file for writing
+with open("books.csv", "w", newline='', encoding="utf-8") as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(["Title", "Price", "Link"])
+
+    for container in book_containers:
+        title = container.h3.a.text
+        price = container.find("div", class_="product_price").p.text
+        link = container.h3.a["href"]
+        print(title, price, link)
+        writer.writerow([title, price, link])
