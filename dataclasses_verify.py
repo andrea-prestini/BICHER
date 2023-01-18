8d1d2cf7dd7a3bbe4215df31cd887590c4fdbf4b
andreaBicher <89815459+andrea-prestini@users.noreply.github.com>
2023-01-18 13:51:26 +0100

    dataclasses post init

---
+++ dataclasses_verify.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+import time
+
+elenco = []
+
+@dataclass
+class Operaio:
+    id: int 
+    nome: str 
+    cognome: str 
+    paese: str 
+
+    def __post_init__(self):
+
+        if self.id in elenco:
+            print("ID già presente...")
+        else:
+            elenco.append(self.id)
+
+
+uno = Operaio(101, "andrea", "prestini", "esine")
+due = Operaio(102, "mario", "mometto", "gambara")
+tre = Operaio(103, "eleonora", "favagrossa", "leno")
+print("Stampo elenco id squadra:")
+for val in elenco:
+    print(val)
+    time.sleep(2)
